f7e1735214670abd40106b537391bd3f95452761
Nick Twyman <nick@conky.org>
2018-03-17 20:15:39 -0700

    Renamed and added MoogLadders sub-directory

--- src/ByeByeFlyGuy-One.cpp
+++ src/ByeByeFlyGuy-One.cpp
@@ -1,5 +1,4 @@
-#include "Template.hpp"
-
+#include "ByeByeFlyGuy-One.hpp"
 
 Plugin *plugin;
 
@@ -10,7 +9,7 @@ void init(rack::Plugin *p) {
 	p->version = TOSTRING(VERSION);
 
 	// Add all Models defined throughout the plugin
-	p->addModel(modelMyModule);
+	p->addModel(modelLadderFilter);
 
 	// Any other plugin initialization may go here.
 	// As an alternative, consider lazy-loading assets and lookup tables when your module is created to reduce startup times of Rack.
